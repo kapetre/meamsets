a15935eb33f0a93cac4b60b5b730c898b6e38b8f
Kat <kapetre@gmail.com>
2018-01-25 16:16:17 -0800

    removed extra streamsets config

--- meamsets/mstreamsetup.sh
+++ meamsets/mstreamsetup.sh
@@ -1,7 +1,7 @@
 # update the hardcoded mapr uid and gid
-find / -user 5000 -exec chown mapr {} \;
-find / -group 5000 -exec chgrp mapr {} \;
+find / -user 1005 -exec chown mapr {} \;
+find / -group 1005 -exec chgrp mapr {} \;
 
 # update the hardcoded streamsets uid and gid
-find / -user 5001 -exec chown streamsets {} \;
-find / -group 6000 -exec chgrp pwner_dev {} \;
+find / -user 1475200018 -exec chown streamsets {} \;
+find / -group 1475200006 -exec chgrp pwner_dev {} \;
